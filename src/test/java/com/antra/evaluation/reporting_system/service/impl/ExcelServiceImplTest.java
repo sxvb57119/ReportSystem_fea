f7c14a894d28312ec6693d2f8faf866498f4704d
Gael Gai <gaiqiyang@gmail.com>
2021-02-07 22:05:11 -0500

    create new test

---
+++ src/test/java/com/antra/evaluation/reporting_system/service/impl/ExcelServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.antra.evaluation.reporting_system.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExcelServiceImplTest {
+
+    @Test
+    void getExcelBodyById() {
+    }
+}
